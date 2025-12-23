4c379b7a94f2f682e6a0fcfe7e3dee6d6be80d8c
oaf-sam <oaf-pureblood-math@duck.com>
2025-12-23 10:16:36 +0900

    Update kadai.js

--- javascript-basic-kadai/kadai/kadai.js
+++ javascript-basic-kadai/kadai/kadai.js
@@ -1,8 +1,8 @@
-// 引数で渡した値を二乗して返す関数
-const square = (num) => {
-  return num * num;
-};
+// id="text" の要素と id="btn" の要素を取得して定数に代入
+const text = document.getElementById('text');
+const btn  = document.getElementById('btn');
 
-// 関数の戻り値をコンソールに出力
-const result = square(10);
-console.log(result); // 100 と表示される
+// ボタンがクリックされたときに文字を変更
+btn.addEventListener('click', () => {
+  text.textContent = 'ボタンをクリックしました！';
+});
