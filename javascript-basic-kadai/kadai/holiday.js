09d29519e2178f76ce74402bbdf17b49a15d0137
oaf-sam <oaf-pureblood-math@duck.com>
2025-12-23 09:42:05 +0900

    Update and rename holiday.js to kadai.js

--- javascript-basic-kadai/kadai/holiday.js
+++
@@ -1,8 +0,0 @@
-// 今日の日付を「YYYY年M月D日」の形式でコンソールに出力する
-const today = new Date();
-
-const year  = today.getFullYear();
-const month = today.getMonth() + 1; // 月は0始まりのため+1
-const date  = today.getDate();
-
-console.log(`${year}年${month}月${date}日`);
---
+++ javascript-basic-kadai/kadai/kadai.js
@@ -0,0 +1,8 @@
+// 引数で渡した値を二乗して返す関数
+const square = (num) => {
+  return num * num;
+};
+
+// 関数の戻り値をコンソールに出力
+const result = square(10);
+console.log(result); // 100 と表示される
